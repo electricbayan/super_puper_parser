35e7ec7610a32219e5375b387032381efc547052
LMECIS <151650293+LMECIS@users.noreply.github.com>
2023-11-22 22:17:45 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+
+def fetch_coordinates(apikey, address):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": apikey,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lon, lat
+apikey = '86712da0-58d8-4d34-9d10-cf7fadf7e03c'
+coords = fetch_coordinates(apikey, "екатеринбург")
+print(coords)
