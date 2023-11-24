83d1da7f71c7867ad3229eed6fdffb020450c482
electric_bayan <a.volkovich@internet.ru>
2023-11-24 23:30:30 +0500

    Adding parse of URL, Name and price

--- parser.py
+++ parser.py
@@ -2,9 +2,10 @@ from bs4 import BeautifulSoup as bs
 import requests
 import asyncio
 from time import sleep
+from pprint import pprint
 
 
-async def parse_names_only():
+def parse_names_only():
     names = []
     for j in range(1, 11):
         url = f'https://ekb.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&p={j}&region=4743&room1=1&room2=1&room3=1&room4=1&room5=1&room6=1'
@@ -12,7 +13,7 @@ async def parse_names_only():
         parser = bs(req.text, 'lxml')
         for i in parser.find_all('span', {'data-mark': "OfferTitle"}):
             names.append(i.text)
-        await asyncio.sleep(3)
+        sleep(3)
     return names
 
 
@@ -27,19 +28,17 @@ def parse_links_only():
             flat_id = link[link.find('flat') + 5:-1]
             req = requests.get(link)
             parser2 = bs(req.text, 'lxml')
-            price = parser2.find('div', {'data-testid': "price-amount"}).text
-            price = price.replace('\\xa', '')
+            price = parser2.find('div', {'data-testid': "price-amount"}).text.replace('\xa0', '').replace(' ', '')
             d[flat_id] = dict()
             d[flat_id]['price'] = price[:-1]
-        sleep(1)
     return d
 
 
 def main():
     task1 = parse_links_only()
     for i in task1.keys():
-        value = task1[i]['price'].replace(' ', '')
-        print(f'{i} - {value}')
+        value = str(task1[i]['price'])
+        print(f'{i} - {int(value)}')
 
 
 if __name__ == '__main__':
