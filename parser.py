aabf25b4279dc0b4be51cf75c49a95446e1ec305
electricbayan <140484512+electricbayan@users.noreply.github.com>
2023-11-27 20:11:45 +0500

    Working parser + full db

--- parser.py
+++ parser.py
@@ -1,55 +1,109 @@
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 import requests
-import asyncio
 from time import sleep, time
-from pprint import pprint
-
-
-def parse_names_only():
-    names = []
-    for j in range(1, 11):
-        url = f'https://ekb.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&p={j}&region=4743&room1=1&room2=1&room3=1&room4=1&room5=1&room6=1'
-        req = requests.get(url)
-        parser = bs(req.text, 'lxml')
-        for i in parser.find_all('span', {'data-mark': "OfferTitle"}):
-            names.append(i.text)
-        sleep(3)
-    return names
+import sqlite3
 
 
 def parse_links_and_address(pages, time_wait=5):
     d = {}
     for j in range(pages):
-        url = f'https://ekb.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&p={j}&region=4743&room1=1&room2=1&room3=1&room4=1&room5=1&room6=1'
+        url = f"https://ekb.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&p={j}&region=4743&room1=1&room2=1&room3=1&room4=1&room5=1&room6=1"
         req = requests.get(url)
-        parser = bs(req.text, 'lxml')
+        parser = BeautifulSoup(req.text, 'lxml')
         for i in parser.find_all('a', {'class': "_93444fe79c--link--eoxce"}):
-            link = i['href']
-            flat_id = link[link.find('flat') + 5:-1]
-            req = requests.get(link)
-            parser2 = bs(req.text, 'lxml')
-            price = parser2.find('div', {'data-testid': "price-amount"}).text.replace('\xa0', '').replace(' ', '')
-            d[flat_id] = dict()
-            d[flat_id]['price'] = price[:-1]
-            d[flat_id]['link'] = link
+            try:
+                link = i['href']
+                flat_id = link[link.find('flat') + 5:-1]
+                req = requests.get(link)
+                parser2 = BeautifulSoup(req.text, 'lxml')
+                price = parser2.find('div', {'data-testid': "price-amount"}).text.replace('\xa0', '').replace(' ', '')
+                d[flat_id] = dict()
+                d[flat_id]['price'] = price[:-1]
+                d[flat_id]['link'] = link
 
-            d[flat_id]['address'] = []
-            for k in parser2.find_all('a', {'data-name': "AddressItem"}):
-                d[flat_id]['address'].append(k.text)
+                d[flat_id]['address'] = []
+                d[flat_id]['description'] = []
+                for k in parser2.find_all('a', {'data-name': "AddressItem"}):
+                    d[flat_id]['address'].append(k.text)
+                for k in parser2.find_all('h1'):
+                    d[flat_id]['description'].append(k.text)
+
+                n = []
+
+                for k in parser2.find_all('div', {'data-name': 'ObjectFactoidsItem'})[:2]:
+                    res = ''
+                    for symb in k.text:
+                        if symb.isdigit() or symb == ',':
+                            res += symb
+                    n.append(res[:-1].replace(',', '.'))
+
+                d[flat_id]['total_square'] = n[0]
+                d[flat_id]['living_square'] = n[1]
+            except Exception:
+                continue
 
         sleep(time_wait)
     return d  # returns dict{flat_id: {price: value}}
 
 
+def parse_one(url):
+    d = {}
+    req = requests.get(url)
+    parser = BeautifulSoup(req.text, 'lxml')
+    price = parser.find('div', {'data-testid': "price-amount"}).text.replace('\xa0', '').replace(' ', '')
+    d['price'] = price[:-1]
+    d['link'] = url
+
+    d['address'] = []
+    d['description'] = []
+    for k in parser.find_all('a', {'data-name': "AddressItem"}):
+        d['address'].append(k.text)
+    for k in parser.find_all('h1'):
+        d['description'].append(k.text)
+
+    n = []
+    for i in parser.find_all('div', {'data-name': 'ObjectFactoidsItem'})[:2]:
+        res = ''
+        for symb in i.text:
+            if symb.isdigit() or symb == ',':
+                res += symb
+        n.append(res[:-1].replace(',', '.'))
+    d['total_square'] = float(n[0])
+    d['living_square'] = float(n[1])
+    return d
+
+
+def write_to_db(d):
+    con = sqlite3.connect('db.sqlite')
+    cur = con.cursor()
+    for i in d.keys():
+        price = d[i]['price']
+        link = d[i]['link']
+        address = ', '.join(d[i]['address'])
+        desc = ', '.join(d[i]['description'])
+        total = d[i]['total_square']
+        living = d[i]['living_square']
+        print(price, link, address, desc, total, living)
+        cur.execute(f"""INSERT INTO flats(flat_id, price, link, address, desc, total_square, living_square) 
+        VALUES({i}, {price}, "{link}", "{address}", "{desc}", "{total}", "{living}")""")
+    con.commit()
+    con.close()
+
+
+
 def main():
     t = time()
-    task1 = parse_links_and_address(7, time_wait=3)
-    for i in task1.keys():
-        print('Flat id:', i)
-        print('Flat price:', task1[i]['price'])
-        print('Address:', ', '.join(task1[i]['address']))
-        print('Cian link:', task1[i]['link'])
-        print('----------')
+    # res = parse_one('https://ekb.cian.ru/sale/flat/292053689/')
+    # print('Name:', ', '.join(res['description']))
+    # print('Flat price:', res['price'])
+    # print('Address:', ', '.join(res['address']))
+    # print('Cian link:', res['link'])
+    # print(res['total_square'])
+    # print(res['living_square'])
+    # print('----------')
+    flats = parse_links_and_address(52, time_wait=5)
+
+    write_to_db(flats)
 
     print('Time:', time() - t)
 
