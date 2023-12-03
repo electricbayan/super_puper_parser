21e56f0b9d5708c8e1b7bb1b8c1142a2d2e49913
electricbayan <140484512+electricbayan@users.noreply.github.com>
2023-12-03 12:56:28 +0500

    Shop parsing, db update

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import requests
 
 def get_flats_data_from_db(cursor):
     """
-    Функция для получения словаря из базы данных
+    Функция для получения словаря из базы данных flats
     Возвращает словарь формата:
-    {flat_id: {price: int, address: str, total_square: float, living_square: float, type: str, district: str}}
+    {flat_id: {price: int, address: str, total_square: float, living_square: float, type: str, district: str, coords: str}}
     """
     d = {}
     for string in cursor.execute("""SELECT * FROM flats"""):
@@ -17,6 +17,21 @@ def get_flats_data_from_db(cursor):
         d[string[0]]['living_square'] = string[6]
         d[string[0]]['type'] = string[7]
         d[string[0]]['district'] = string[3].split(', ')[2]
+        d[string[0]]['coords'] = string[8].replace(';', ',')
+    return d
+
+
+def get_shops_data_from_db(cursor):
+    """
+    Функция для получения словаря из базы данных shops
+    Возвращает словарь формата:
+    {address: {coords: str, type: str}}
+    """
+    d = {}
+    for string in cursor.execute("""SELECT * FROM shops"""):
+        d[string[0]] = {}
+        d[string[0]]['coords'] = string[1].replace(';', ',')
+        d[string[0]]['type'] = string[2]
     return d
 
 
