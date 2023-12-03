d1db8bea3b833c4a9c70083b7c906b53e763fb4e
Alexandr <sasha.chwr@gmail.com>
2023-12-03 14:03:50 +0500

    jhvhgyb

--- gauss.py
+++ gauss.py
@@ -23,24 +23,20 @@ def get_apartment_data_from_db(cursor):
     return data
 
 def plot_price_distribution(apartments):
-    prices = [apartment['price'] / 1e6 for apartment in apartments.values() if apartment['price'] <= 30e6]  # Фильтр цен до 30M
+    prices = [apartment['price'] / 1e6 for apartment in apartments.values() if apartment['price'] <= 30e6]
 
-    # Ядерная оценка плотности для цен на квартиры
     kde = KernelDensity(bandwidth=1.0, kernel='gaussian')
     kde.fit(np.array(prices).reshape(-1, 1))
     x_values = np.linspace(0, 30, 1000)
     y_values = np.exp(kde.score_samples(x_values.reshape(-1, 1)))
 
-    # Сглаживание линии с увеличением количества точек
     x_smooth = np.linspace(x_values.min(), x_values.max(), 5000)
     spl = make_interp_spline(x_values, y_values, k=3)
     y_smooth = spl(x_smooth)
 
-    # Построение графика сглаженной линии цен на квартиры без точек
     plt.plot(x_smooth, y_smooth, 'r-')
 
-    # Построение гистограммы цен на квартиры без точек
-    bins = np.arange(0, 30.5, 1)  # Шаг 1M, максимальная цена 30M
+    bins = np.arange(0, 30.5, 1)
     n, _ = np.histogram(prices, bins=bins)
     plt.plot(bins[:-1], n, linestyle='-')
 
@@ -54,12 +50,9 @@ def plot_price_distribution(apartments):
 
     plt.show()
 
-# Подключение к базе данных SQLite
 con = sqlite3.connect('db.sqlite')
 cur = con.cursor()
 
-# Извлечение данных о квартирах из базы данных
 apartment_data = get_apartment_data_from_db(cur)
 
-# Построение графика распределения цен с использованием ядерной оценки плотности (до 30M) без точек
 plot_price_distribution(apartment_data)
